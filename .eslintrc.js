4820fd3985a4d8cea2b55dde4bee4c2a5ae48bd5
@bkawk <email.will.in.china@gmail.com>
2018-04-22 09:58:02 +0800

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "extends": "google",
+    "env": {
+        "browser": false,
+        "node": true,
+        "es6": true
+    },
+};
--- index.js
+++ index.js
@@ -1,22 +1,20 @@
-"use strict";
+'use strict';
 const request = require('request');
-const RSI = require('technicalindicators').RSI;
 const moment = require('moment');
+// const RSI = require('technicalindicators').RSI;
 
-let priceArray = [];
-let rsiArray = [];
-let timeFrame = '1h';
+// let priceArray = [];
+// let rsiArray = [];
 
-setInterval(() => { 
+setInterval(() => {
     // TODO: Make sure there is a close price
-        getPrice(timeFrame)
+        getPrice('1h', 'BTCUSD')
         .then((priceData) => {
             console.log(priceData);
         })
         .catch((error) => {
             console.log(Error(error));
-        })
-        
+        });
     //     if (priceArray.length >= 7) {
     //         priceArray.reverse();
     //         if(priceData != priceArray[1]){
@@ -25,7 +23,7 @@ setInterval(() => {
     //             priceArray.reverse();
     //             calculateRSI(priceArray);
     // }
-    
+
     // } else {
     //     priceArray.reverse();
     //         if(priceData != priceArray[1]){
@@ -36,22 +34,31 @@ setInterval(() => {
     // }
     }, 5000);
 
-
-//     var milliseconds = 1000;
-// 
-
-    function getPrice(timeFrame) {
+/**
+ * Request price data from Bitfinex
+ * @param {string} timeFrame The time frame to request.
+ * @param {string} pair The pair to request
+ * @return {object} the price data
+ */
+    function getPrice(timeFrame, pair) {
         return new Promise(function(resolve, reject) {
             let url = 'https://api.bitfinex.com/v2';
-            request.get(`${url}/candles/trade:${timeFrame}:tBTCUSD/last`, (error, response, data) => {
+            request.get(`${url}/candles/trade:${timeFrame}:t${pair}/last`,
+            (error, response, data) => {
                 let price = JSON.parse(data);
-                let time = moment.utc(price[0]).local().format("DD/MM/YYYY HH:mm:ss");
-                if (!error) { 
-                    resolve({open:price[1], close:price[2], high: price[3], low:price[4], volume:price[5], time:time});
+                let time = moment.utc(price[0]).local().format('HH:mm');
+                if (!error) {
+                    resolve({
+                        open: price[1],
+                        close: price[2],
+                        high: price[3],
+                        low: price[4],
+                        volume: price[5],
+                        time: time,
+                    });
                 } else {
                     reject(error);
                 }
-            })
-        })
+            });
+        });
     }
-        
