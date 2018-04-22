7a8995f1a13be359a0cbf381a7c41e9778acd856
@bkawk <email.will.in.china@gmail.com>
2018-04-22 17:11:29 +0800

    live and historic data

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# divergence
+# Divergence Detector
+
+### Prerequisites
+
+First, install [npm](https://www.npmjs.com) (we assume you have pre-installed [node.js](https://nodejs.org)).
+
+### Setup
+
+Install dependencies
+
+    npm install
+
+Start the app
+
+    npm run now
--- index.js
+++ index.js
@@ -6,19 +6,43 @@ const RSI = require('technicalindicators').RSI;
 let priceArray = [];
 let rsiArray = [];
 
-setInterval(() => {
-        getPrice('1m', 'BTCUSD')
-        .then((priceData) => {
-            return updatePriceArray(priceData);
-        })
-        .then((priceArray) => {
-            calculateRSI(priceArray);
-        })
-        .catch((error) => {
-            console.log(Error(error));
-        });
-    }, 5000);
+(() => {
+    console.log('fetching historic data');
+    getPrice('1m', 'BTCUSD', 'hist')
+    .then((historicPriceData) => {
+        priceArray = historicPriceData;
+        setInterval(() => {
+            getPrice('1m', 'BTCUSD', 'last')
+            .then((priceData) => {
+                priceArray.push(priceData);
+                return calculateRSI(priceArray);
+            })
+            .then((data) => {
+                let price = data[0].reverse();
+                let rsi = data[1].reverse();
+                return detectDivergence(price, rsi);
+            })
+            .then((divergence) => {
+                console.log(`Divergence Found: ${divergence}`);
+            })
+            .catch((error) => {
+                console.log(Error(error));
+            });
+        }, 5000);
+    });
+})();
 
+/**
+ * Detet divegence
+ * @param {object} price The price array data
+ * @param {object} rsi The rsi array data
+ * @return {boolean} hasa divergence been found true/false
+ */
+function detectDivergence(price, rsi) {
+    return new Promise(function(resolve, reject) {
+        resolve(false);
+    });
+}
 
 /**
  * calculate RSI
@@ -26,68 +50,56 @@ setInterval(() => {
  * @return {object} the RSI Array
  */
 function calculateRSI(priceArray) {
-    if(priceArray && priceArray.length >= 15){
+    return new Promise(function(resolve, reject) {
         let closeArray = [];
         priceArray.forEach((entry) => {
             closeArray.push(entry.close);
         });
-        if(closeArray && closeArray.length >=15) {
-            rsiArray = RSI.calculate({values:closeArray, period:14});
-            console.log(closeArray);
-            console.log(rsiArray);
-        }
-    } else {
-        let timeToWait = (15 -priceArray.length) 
-        console.log(`Need more data please wait ${timeToWait} min`)
-    }
-}
-        
-/**
- * Request price data from Bitfinex
- * @param {object} priceData The proce data
- * @return {object} the price data
- */
-function updatePriceArray(priceData) {
-    return new Promise(function(resolve, reject) {
-        if (priceArray.filter(
-            (item) => (item.time === priceData.time)
-        ).length == 0 ) {
-            priceArray.push(priceData);
-        } else {
-            priceArray = priceArray.filter(function(el) {
-                return el.time !== priceData.time;
-            });
-            priceArray.push(priceData);
-        }
-        if (priceArray.length == 30) {
-            priceArray.shift();
-        }
-        resolve(priceArray);
+        rsiArray = RSI.calculate({values: closeArray, period: 14});
+        resolve([closeArray, rsiArray]);
     });
 }
 
 /**
  * Request price data from Bitfinex
  * @param {string} timeFrame The time frame to request.
- * @param {string} pair The pair to request
+ * @param {string} pair The pair to request like BTCUSD
+ * @param {string} mode last or hist
  * @return {object} the price data
  */
-function getPrice(timeFrame, pair) {
+function getPrice(timeFrame, pair, mode) {
     return new Promise(function(resolve, reject) {
         let url = 'https://api.bitfinex.com/v2';
-        request.get(`${url}/candles/trade:${timeFrame}:t${pair}/last`,
+        request.get(`${url}/candles/trade:${timeFrame}:t${pair}/${mode}`,
         (error, response, data) => {
             let price = JSON.parse(data);
-            let time = moment.utc(price[0]).local().format('HH:mm:ss');
             if (!error) {
-                resolve({
-                    open: price[1],
-                    close: price[2],
-                    high: price[3],
-                    low: price[4],
-                    volume: price[5],
-                    time: time,
-                });
+                if (mode == 'last') {
+                    let time = moment.utc(price[0]).local().format('HH:mm:ss');
+                    resolve({
+                        open: price[1],
+                        close: price[2],
+                        high: price[3],
+                        low: price[4],
+                        volume: price[5],
+                        time: time,
+                    });
+                };
+                if (mode == 'hist') {
+                    let historicDataArray = [];
+                    price.forEach((item) => {
+                    let time = moment.utc(item[0]).local().format('HH:mm:ss');
+                        historicDataArray.push({
+                            open: item[1],
+                            close: item[2],
+                            high: item[3],
+                            low: item[4],
+                            volume: item[5],
+                            time: time,
+                        });
+                    });
+                    resolve(historicDataArray);
+                };
             } else {
                 reject(error);
             }
