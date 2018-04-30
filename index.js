0474999d269c5e09238a93205eda4876bdac48f7
@bkawk <email.will.in.china@gmail.com>
2018-04-30 17:07:19 +0800

    lint

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 const fs = require('fs');
 const moment = require('moment');
 const RSI = require('@solazu/technicalindicators').RSI;
-const ws = require('ws')
-var util = require('util');
+const Ws = require('ws');
+const util = require('util');
 
 let bitfinexData = [];
 let bitfinexSubscriptions = [];
@@ -20,16 +20,16 @@ createBitfinexSubscriptions()
 .then(() => {
     let waitingForHour = 0;
     setInterval(() => {
-        if(waitingForHour == 0){
+        if (waitingForHour == 0) {
             console.log(`Waiting for the top of the hour before scanning`);
             waitingForHour = 1;
         }
-        var minutes = new Date().getMinutes();
-        var seconds = new Date().getSeconds();
-        //if (minutes == 0 && seconds == 0 ){
+let minutes = new Date().getMinutes(); // eslint-disable-line no-unused-vars
+let seconds = new Date().getSeconds(); // eslint-disable-line no-unused-vars
+        // if (minutes == 0 && seconds == 0 ){
             console.log(`Top of the hour is now`);
             scanData();
-        //}
+        // }
     }, 10000);
 })
 .catch((error) => {
@@ -39,10 +39,24 @@ createBitfinexSubscriptions()
 
 /**
  * Get Bitfinex Data
+ * @return {promise} an empty promise
  */
-function createBitfinexSubscriptions(){
+function createBitfinexSubscriptions() {
     return new Promise(function(resolve, reject) {
-        let timeFrames = ['1h', '2h', '3h', '4h', '5h', '6h', '7h', '8h', '9h', '10h', '11h', '12h'];
+        let timeFrames = [
+            '1h',
+            '2h',
+            '3h',
+            '4h',
+            '5h',
+            '6h',
+            '7h',
+            '8h',
+            '9h',
+            '10h',
+            '11h',
+            '12h',
+        ];
         let pairs = [
             'EOSUSD',
             'ZRXUSD',
@@ -99,36 +113,47 @@ function createBitfinexSubscriptions(){
 
         timeFrames.forEach((timeFrames) => {
             pairs.forEach((pairs) => {
-                bitfinexSubscriptions.push({event: 'subscribe', channel: 'candles', key: `trade:${timeFrames}:t${pairs}`})
+                bitfinexSubscriptions.push({
+                    event: 'subscribe',
+                    channel: 'candles',
+                    key: `trade:${timeFrames}:t${pairs}`,
+                });
             });
         });
         resolve();
-    })
+    });
 }
 
 /**
  * Get Bitfinex Data
+ * @return {promise} an empty promise
  */
 function getBitfinexData() {
     return new Promise(function(resolve, reject) {
         let initialDataComplete = 0;
-        const w = new ws('wss://api.bitfinex.com/ws/2')
+        const w = new Ws('wss://api.bitfinex.com/ws/2');
         w.on('message', (msg) => {
             let data = JSON.parse(msg);
-            if (data.event == "subscribed" && data.channel == "candles") {
-                let key = data.key.split(":");
-                bitfinexData.push({pair: key[2], timeFrame: key[1], chanId: data.chanId, data: []});
+            if (data.event == 'subscribed' && data.channel == 'candles') {
+                let key = data.key.split(':');
+                bitfinexData.push({
+                    pair: key[2],
+                    timeFrame: key[1],
+                    chanId: data.chanId,
+                    data: [],
+                });
             } else if (data[0] != undefined && data[1] != 'hb') {
                 let chanId = data[0];
                 let price = data[1];
                 let time = moment.unix(price[0]).local().format('HH:mm');
-                var pair = bitfinexData.filter((obj) => {
+                let pair = bitfinexData.filter((obj) => {
                     return obj.chanId === chanId;
                 });
                 let pairData = pair[0].data;
                 if (price.length > 6) {
                     price.forEach((price)=>{
-                        let time = moment.unix(price[0]).local().format('HH:mm');
+                        let time = moment.unix(
+                            price[0]).local().format('HH:mm');
                         pairData.push({
                             open: price[1],
                             close: price[2],
@@ -137,7 +162,7 @@ function getBitfinexData() {
                             volume: price[5],
                             time: time,
                         });
-                    }) 
+                    });
                 } else {
                     pairData.push({
                         open: price[1],
@@ -151,24 +176,26 @@ function getBitfinexData() {
                 if (pairData.length >= 150) {
                     pairData.shift();
                 }
-                if(bitfinexData.length == bitfinexSubscriptions.length && initialDataComplete == 0){
+                if (bitfinexData.length == bitfinexSubscriptions.length &&
+                    initialDataComplete == 0) {
                     initialDataComplete = 1;
                     console.log(`Initial Bitfinex data complete`);
                     console.log(`Listening for more data on websockets`);
                     resolve();
                 }
             }
-        })
+        });
         w.on('open', () => {
             bitfinexSubscriptions.forEach((pairs, i) => {
-                w.send(JSON.stringify(bitfinexSubscriptions[i]))
-            })
-        })
-    })
+                w.send(JSON.stringify(bitfinexSubscriptions[i]));
+            });
+        });
+    });
 }
 
 /**
  * start scanner
+ * @return {promise} an empty promise
  */
 function scanData() {
     return new Promise(function(resolve, reject) {
@@ -177,25 +204,38 @@ function scanData() {
         dataArray.forEach((results)=>{
             calculateRSI(results.data)
             .then((rsiAndPrice) => {
-                return createColumns(rsiAndPrice[0], rsiAndPrice[1], results.timeFrame, results.pair);
+                return createColumns(
+                    rsiAndPrice[0],
+                    rsiAndPrice[1],
+                    results.timeFrame,
+                    results.pair
+                );
             })
             .then((divergence) => {
-                saveData('divergence', `{pair:${divergence.pair}, timeFrame:${divergence.timeFrame}, period:${divergence.data}, direction:${divergence.direction}, column:${JSON.stringify(divergence.column)}}`);
+                saveData(
+                    'divergence',
+                    `{pair:${divergence.pair},
+                        timeFrame:${divergence.timeFrame},
+                        period:${divergence.data},
+                        direction:${divergence.direction},
+                        column:${JSON.stringify(divergence.column)}}`
+                    );
             })
             .catch((error) => {
                 console.log(error);
             });
-        })
-    })
+        });
+    });
 }
 
 
 /**
  * Spike Detector
  * Part of being able to detect a divergence is to know where the numbers spike
- * A spike is where the numbers before and after the terget are both higher or lower than the target
+ * A spike is where the numbers before and after
+ * the terget are both higher or lower than the target
  * @param {number} left The value to the left of target
- * @param {number} head The tagets value
+ * @param {number} target The tagets value
  * @param {number} right The value to the right of target
  * @return {string} the string indicating direction
  */
@@ -234,8 +274,26 @@ function createColumns(price, rsi, timeFrame, pair) {
             }
         });
 
-        let periods = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
-        if (column.length > 1){
+        let periods = [
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9,
+            10,
+            11,
+            12,
+            13,
+            14,
+            15,
+            16,
+            17,
+            18,
+        ];
+        if (column.length > 1) {
             Promise.all([
                 divergenceStrategy(column, pair, timeFrame, periods),
             ])
@@ -265,7 +323,7 @@ function createColumns(price, rsi, timeFrame, pair) {
 function divergenceStrategy(column, pair, timeFrame, period) {
     return new Promise(function(resolve, reject) {
         period.forEach((data) => {
-            let i = data + 2; 
+            let i = data + 2;
                 if (
                     i <= 16 &&
                     column[2].priceSpike == 'up' &&
@@ -275,30 +333,29 @@ function divergenceStrategy(column, pair, timeFrame, period) {
                     column[i].priceValue < column[2].priceValue &&
                     column[i].rsiValue > column[2].rsiValue
                 ) {
-                    let divergencePeriod = data;
                     let firstPriceSpikeValue = column[2].priceValue;
                     let secondPriceSpikeValue = column[i].priceValue;
                     let firstRsiSpikeValue = column[2].rsiValue;
                     let secondRsiSpikeValue = column[i].rsiValue;
-                    
-                    var x;
-                    for (x = 0; x <= i.length; x++) { 
-                        let priceSlope = slope(period, firstPriceSpikeValue, secondPriceSpikeValue);
-                        let rsiSlope = slope(period, firstRsiSpikeValue, secondRsiSpikeValue);
-                        if(priceSlope && rsiSlope){
+
+                    let x;
+                    for (x = 0; x <= i.length; x++) {
+                        let priceSlope = slope(period,
+                            firstPriceSpikeValue,
+                            secondPriceSpikeValue);
+                        let rsiSlope = slope(period,
+                            firstRsiSpikeValue,
+                            secondRsiSpikeValue);
+                        if (priceSlope && rsiSlope) {
                             console.log(x);
-                            if(x == period.length){
-                                console.log('yay we have a divergence')
+                            if (x == period.length) {
+                                console.log('yay we have a divergence');
                             }
                         } else {
                             break;
                         }
                     }
 
-                    function slope(period, firstValue, secondValue){
-                        let slopeValue = ((firstValue - secondValue) / Math.abs(period - 2)) * period
-                        return (secondValue < slopeValue)
-                    }
                     resolve({
                         divergence: true,
                         period: data,
@@ -329,11 +386,24 @@ function divergenceStrategy(column, pair, timeFrame, period) {
         });
     });
 }
-
-
+/**
+ * Calculate Slope
+ * Is the price or rsi value higher than the slope value
+ * @param {number} period The proce data
+ * @param {number} firstValue The proce data
+ * @param {number} secondValue The proce data
+ * @return {boolean} true or false
+ */
+function slope(period, firstValue, secondValue) {
+    let slopeValue = (
+        (firstValue - secondValue) /
+        Math.abs(period - 2)) * period;
+    return (secondValue < slopeValue);
+}
 /**
  * Calculate RSI
- * From the historic and live data, calculate the RSI and return the last 9 as 2 arrays
+ * From the historic and live data, calculate the
+ * RSI and return the last 9 as 2 arrays
  * @param {object} priceArray The proce data
  * @return {object} the RSI Array
  */
@@ -341,7 +411,7 @@ function calculateRSI(priceArray) {
     return new Promise(function(resolve, reject) {
         let closeArray = [];
         priceArray.forEach((entry) => {
-            if(entry.close){
+            if (entry.close) {
                 closeArray.push(entry.close);
             }
         });
@@ -351,21 +421,20 @@ function calculateRSI(priceArray) {
             reversedInput: true,
         };
         let rsiArray = (RSI.calculate(inputRSI));
-        if(closeArray.length > 0 && rsiArray.length > 0){
+        if (closeArray.length > 0 && rsiArray.length > 0) {
             resolve([closeArray.slice(0, 20), rsiArray.slice(0, 20)]);
         }
-        
     });
 }
 
 /**
  * Save Data
  * Save the data to disk
- * @param {object} name The proce data
+ * @param {string} name The proce data
+ * @param {object} data The proce data
  */
 function saveData(name, data) {
-        console.log("saveData");
-        var log_file = fs.createWriteStream(__dirname + `/${name}.log`, {flags : 'a'});
-        var log_stdout = process.stdout;
-        log_file.write(util.format(data) + '\n');
+        let logFile = fs.createWriteStream(
+            __dirname + `/${name}.log`, {flags: 'a'});
+        logFile.write(util.format(data) + '\n');
 }
