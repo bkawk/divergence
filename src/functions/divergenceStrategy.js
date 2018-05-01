7ce9913e74ce07671e4c136d59266e4ebe235511
colinskeep <colinjinskeep@gmail.com>
2018-04-30 21:03:45 -0700

    filter false diversions

--- src/functions/divergenceStrategy.js
+++ src/functions/divergenceStrategy.js
@@ -12,8 +12,8 @@ const slope = require('./slope');
  */
 module.exports = function divergenceStrategy(column, pair, timeFrame, period) {
     return new Promise(function (resolve, reject) {
-        period.forEach((data) => {
-            let i = data + 2;
+        period.forEach((period) => {
+            let i = period;
             if (
                 i <= 16 &&
                 column[2].priceSpike == 'up' &&
@@ -27,33 +27,30 @@ module.exports = function divergenceStrategy(column, pair, timeFrame, period) {
                 let secondPriceSpikeValue = column[i].priceValue;
                 let firstRsiSpikeValue = column[2].rsiValue;
                 let secondRsiSpikeValue = column[i].rsiValue;
-
                 let x;
-                for (x = 0; x <= i.length; x++) {
-                    let priceSlope = slope(period,
+                for (x = 2; x <= i; x++) {
+                    let priceSlope = slope(i,
                         firstPriceSpikeValue,
                         secondPriceSpikeValue);
-                    let rsiSlope = slope(period,
+                    let rsiSlope = slope(i,
                         firstRsiSpikeValue,
                         secondRsiSpikeValue);
                     if (priceSlope && rsiSlope) {
-                        console.log(x);
-                        if (x == period.length) {
-                            console.log('yay we have a divergence');
+                        if (x == i) {
+                            resolve({
+                                divergence: true,
+                                period: i,
+                                direction: 'bearish',
+                                pair: pair,
+                                timeFrame: timeFrame,
+                                data: column,
+                            });
                         }
                     } else {
                         break;
                     }
                 }
 
-                resolve({
-                    divergence: true,
-                    period: data,
-                    direction: 'bearish',
-                    pair: pair,
-                    timeFrame: timeFrame,
-                    data: column,
-                });
             }
             if (
                 i <= 15 &&
@@ -64,14 +61,33 @@ module.exports = function divergenceStrategy(column, pair, timeFrame, period) {
                 column[i].priceValue > column[2].priceValue &&
                 column[i].rsiValue < column[2].rsiValue
             ) {
-                resolve({
-                    divergence: true,
-                    period: data,
-                    direction: 'bullish',
-                    pair: pair,
-                    timeFrame: timeFrame,
-                    data: column,
-                });
+                let firstPriceSpikeValue = column[2].priceValue;
+                let secondPriceSpikeValue = column[i].priceValue;
+                let firstRsiSpikeValue = column[2].rsiValue;
+                let secondRsiSpikeValue = column[i].rsiValue;
+                let x;
+                for (x = 2; x <= i; x++) {
+                    let priceSlope = slope(i,
+                        firstPriceSpikeValue,
+                        secondPriceSpikeValue);
+                    let rsiSlope = slope(i,
+                        firstRsiSpikeValue,
+                        secondRsiSpikeValue);
+                    if (priceSlope && rsiSlope) {
+                        if (x == i) {
+                            resolve({
+                                divergence: true,
+                                period: i,
+                                direction: 'bullish',
+                                pair: pair,
+                                timeFrame: timeFrame,
+                                data: column,
+                            });
+                        }
+                    } else {
+                        break;
+                    }
+                }
             }
         });
     });
