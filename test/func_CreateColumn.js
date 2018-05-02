eefbc74705738fc47b376fb115eee93f7137f770
cipto <oci_beken@yahoo.co.id>
2018-05-02 21:12:44 +0700

    Complete the task with some notes,all command working, updated readme

--- test/func_CreateColumn.js
+++ test/func_CreateColumn.js
@@ -1,35 +1,30 @@
-const sinon = require('sinon');
 const createColumn = require('../src/functions/createColumns');
-const withStatus = require('promise-with-status')(Promise);
 var chai = require('chai'),
-    should = chai.should,
     expect = chai.expect,
     assert = chai.assert;
 
 describe('Create Column tests', function () {
-    var price = Array(1);
-    price.push(16.82);
-    var rsi = Array(1);
-    rsi.push(39.21);
-    var timeFrame = "1h";
-    var pair = "tEOSUSD";
+    const price = [16.82];
+    const rsi =[39.21];
+    const timeFrame = "1h";
+    const pair = "tEOSUSD";
 
-    it("Should return promise pending with current sample data", function () {
-        var promise = createColumn(price, rsi, timeFrame, pair);
+    it('Should return promise pending with current sample data', function () {
+        const promise = createColumn(price, rsi, timeFrame, pair);
         promise.then(() => {
             expect(promise.status).to.be.eq("Pending");
             done();
         });
     });
 
-    it("Should call divergance strategy linear with column length", function () {
+    it('Should call divergance strategy linear with column length', function () {
 
 
     });
-    it("Column called should be maxed 18 even Price length is higher", function () {
+    it('Column called should be maxed 18 even Price length is higher', function () {
 
     });
-    it("When Column empty should not give any data", function () {
+    it('When Column empty should not give any data', function () {
 
     });
 });
---
+++ test/serv_Bitfinex.js
@@ -0,0 +1,37 @@
+var chai = require('chai'),
+    should = chai.should,
+    expect = chai.expect,
+    assert = chai.assert,
+    deep = chai.deep;
+var chaiAsPromised = require('chai-as-promised');
+//calling should bef, solved chaiaspromised bug
+chai.should();
+chai.use(chaiAsPromised);
+
+const Bitfinex = require('../src/services/bitfinex');
+
+describe('Bitfinex tests', function () {
+    const timeFrames = ["1h", "2h"];
+    const pairs = ["EOSUSD", "ZRXUSD"];
+    const apiUrl = "Dummy";
+    let bitfinexService = new Bitfinex(timeFrames, pairs, apiUrl);
+
+    it("createBitfinexSubscriptions should return resolved promise", function () {
+        return assert.isFulfilled(bitfinexService.createBitfinexSubscriptions(),"resolved");
+    });
+    it("createBitfinexSubscriptions Should return matrix of timeFrame and Pair", function () {
+        var expectedResults = [
+            { channel: "candles", event: "subscribe", key: "trade:1h:tEOSUSD" },
+            { channel: "candles", event: "subscribe", key: "trade:1h:tZRXUSD" },
+            { channel: "candles", event: "subscribe", key: "trade:2h:tEOSUSD" },
+            { channel: "candles", event: "subscribe", key: "trade:2h:tZRXUSD" }];
+            
+        return bitfinexService.createBitfinexSubscriptions().should.eventually.deep.equal(expectedResults);
+
+    });
+
+});
+
+
+
+
