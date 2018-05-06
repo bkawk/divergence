a6bb74c9003c57915f35ec9a4009222e02faa718
cipto <oci_beken@yahoo.co.id>
2018-05-06 08:38:43 +0700

    add more unit test

--- test/func_isJson.js
+++ test/func_isJson.js
@@ -1,12 +1,13 @@
 const isJson = require('../src/functions/isJson');
-const chai = require('chai'),
-    should = chai.should,
-    expect = chai.expect,
-    assert = chai.assert;
-
+const chai = require('chai');
+const expect = chai.expect;
 describe('Test Json', () => {
     it('Should return true if the json is valid', () => {
-        const json = {test: "test"};
+        const json = {test: 'test'};
         expect(isJson(json)).to.be.eq(true);
     });
-})
+    it('Should return false if the json is invalid', () => {
+        const json = 'InvalidJSON';
+        expect(isJson(json)).to.be.eq(true);
+    });
+});
